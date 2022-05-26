6597939f41748145350f3f853091e1c432a2c878
Moe Thant Kyaw <moethantk@gmail.com>
2022-05-26 19:58:11 +0700

    s

--- moneyapp/ui/uipy/budget_wid.py
+++ moneyapp/ui/uipy/budget_wid.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QLabel, QProgressBar, QPushButton,
     QSizePolicy, QWidget)
-import resource_rc
+from .resource_rc import *
 
 class Ui_Form(object):
     def setupUi(self, Form):
